6113fd12468bc7b510251bb76633a208f58cd8d5
Rafael Santos <rafa.05125@gmail.com>
2025-02-01 02:55:38 -0300

    1044

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,21 +10,13 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         scanner.useLocale(Locale.US);
 
-        double[] numbers = new double[3];
-        for (int i = 0; i < 3; i++) {
-            numbers[i] = scanner.nextDouble();
-        }
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
 
-        double a = numbers[0];
-        double b = numbers[1];
-        double c = numbers[2];
-
-        if (a + b > c && a + c > b && b + c > a) {
-            double perimeter = a + b + c;
-            System.out.printf("Perimetro = %.1f%n", perimeter);
-        } else {
-            double area = ((a + b) * c) / 2;
-            System.out.printf("Area = %.1f%n", area);
+        if (a % b == 0 || b % a == 0){
+            System.out.println("Sao Multiplos");
+        }else {
+            System.out.println("Nao sao Multiplos");
         }
     }
 }
