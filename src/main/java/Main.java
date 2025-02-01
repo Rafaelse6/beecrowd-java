02e3d314174507a26027736e2de02075ec1adc3c
Rafael Santos <rafa.05125@gmail.com>
2025-01-31 21:46:13 -0300

    1050

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 
@@ -8,23 +9,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
 
-        int[] numbers = new int[3];
-        for (int i = 0; i < 3; i++){
-            numbers[i] = scanner.nextInt();
-        }
+        Map<Integer, String> ddd = new HashMap<>();
+        ddd.put(61, "Brasilia");
+        ddd.put(71, "Salvador");
+        ddd.put(11, "Sao Paulo");
+        ddd.put(21, "Rio de Janeiro");
+        ddd.put(32, "Juiz de Fora");
+        ddd.put(19, "Campinas");
+        ddd.put(27, "Vitoria");
+        ddd.put(31, "Belo Horizonte");
 
-        int []original = numbers.clone();
+        int cityCode = scanner.nextInt();
 
-        Arrays.sort(numbers);
-
-        for (int num: numbers){
-            System.out.println(num);
-        }
-
-        System.out.println();
-
-        for (int num: original){
-            System.out.println(num);
-        }
+        System.out.println(ddd.getOrDefault(cityCode, "DDD nao cadastrado"));
     }
 }
